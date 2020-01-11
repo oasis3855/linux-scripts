4fc633205f85dfd2c94693a620260eee0128da06
oasis3855 <oasis3855@users.noreply.github.com>
2020-01-11 04:00:36 +0000

    change-desktop-image : (Ubuntu 18.04) pgrep gnome-sessionでPIDが2つ以上返される場合の対処
    git-svn-id: svn+ssh://oasis.halfmoon.jp/home/tmg1136-inue/var/svn/linux-scripts@4 16e5acf2-a0d0-e711-ab87-525400268553

--- gnome-change-desktop-image/change-desktop-image.sh
+++ gnome-change-desktop-image/change-desktop-image.sh
@@ -7,6 +7,10 @@
 # usage : 引数に画像ファイルが格納されたディレクトリを指定
 #
 # 定期的に変更する場合は crontab -e でこのスクリプトを動作させる
+#
+# 30分ごとの例 : */30 * * * * /usr/local/bin/change-desktop-image.sh /home/USER/Pictures/Wallpapers 1> /dev/null 2>&1
+# syslogに記録する例：*/30 * * * * /usr/local/bin/change-desktop-image.sh /home/USER/Pictures/Wallpapers 2>&1 | logger -t change_wallpaper -p local0.info
+#
 
 
 # 引数が1つ以外の場合、説明文を画面表示して終了
@@ -18,9 +22,10 @@ fi
 
 DIR=$1
 
-PID=$(pgrep gnome-session)
+# PID番号が2つ返される場合、1行目はroot(gdm)、2行目はユーザ (Ubuntu 18.04対応)
+PID=$(pgrep gnome-session | tail -n1)
+# 例「DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/121/bus」
 export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
-
 
 # 引数がディレクトリ名の場合は、デスクトップ画像を変更する"
 
@@ -29,14 +34,15 @@ then
   COUNT=$(find $DIR -maxdepth 1 -name '*.jpg' | wc -l)
   if [ $COUNT -le 0 ]
   then
-    echo "$0 : warning, no jpeg file in directory($DIR)."
+    echo "($0) warn no pic file : dir = $DIR , PID = $PID , dbus = $DBUS_SESSION_BUS_ADDRESS"
     exit 1
   else
     PIC=$(find $DIR -maxdepth 1 -name '*.jpg' | shuf -n1)
     gsettings set org.gnome.desktop.background picture-uri "file://$PIC"
+    echo "($0) info : dir = $DIR , PID = $PID , dbus = $DBUS_SESSION_BUS_ADDRESS , pic = $PIC"
   fi
 else
-  echo "$0 : warning, not a directory($DIR)."
+  echo "($0) warn not a dir : dir = $DIR , PID = $PID , dbus = $DBUS_SESSION_BUS_ADDRESS"
   exit 1
 fi
 
